68268cfb7d6d5ca0310a0b5279fc9f2bd911a397
xboudsady <boudsadyx@gmail.com>
2018-12-26 21:30:09 -0800

    Completed Strings

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,58 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Seth'
+age = 35
 
 # String Formatting
 
 # String Methods
+
+# Arguments by position
+# print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# F-Strings (3.6+)
+# print(f'Hello, my name is {name} and I am {age}')
+
+# String Methods
+
+s = 'hello world'
+
+# Captilaize string
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lower
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get length
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'h'
+print(s.count(sub))
+
+# Starts with
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('d'))
+
+# Split into a list
+print(s.split())
+
+# Find position
+print(s.find('r'))
+
+# Is all alphanumeric
+print(s.isalnum())
+
+# Is all numeric
+print(s.isnumeric())
